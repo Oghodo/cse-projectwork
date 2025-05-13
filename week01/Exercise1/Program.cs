16a10d90dcf3022aa2d52442785b8860e20103ba
oghodo omoregbe <omoregbeoghodo54@gmail.com>
2025-05-14 00:41:53 +0100

    Changes made

--- week01/Exercise1/Program.cs
+++ week01/Exercise1/Program.cs
@@ -5,5 +5,10 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the Exercise1 Project.");
+        Console.WriteLine("This is in c#.");
+
+        Console.WriteLine("what is your favorite color?");
+        string color = Console.ReadLine();
+        Console.WriteLine(color);
     }
 }
