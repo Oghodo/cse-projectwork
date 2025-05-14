96e283e283d1ba662422a5d434cd3301cb5a89f5
oghodo omoregbe <omoregbeoghodo54@gmail.com>
2025-05-14 01:44:02 +0100

    changes made

--- week01/Exercise2/Program.cs
+++ week01/Exercise2/Program.cs
@@ -2,8 +2,69 @@ using System;
 
 class Program
 {
-    static void Main(string[] args)
+    static void Main()
     {
-        Console.WriteLine("Hello World! This is the Exercise2 Project.");
+        // Ask for user input
+        Console.Write("Enter your grade percentage: ");
+        string input = Console.ReadLine();
+        int grade = int.Parse(input);
+
+        string letter = "";
+        string sign = "";
+
+        // Determine the base letter grade
+        if (grade >= 90)
+        {
+            letter = "A";
+        }
+        else if (grade >= 80)
+        {
+            letter = "B";
+        }
+        else if (grade >= 70)
+        {
+            letter = "C";
+        }
+        else if (grade >= 60)
+        {
+            letter = "D";
+        }
+        else
+        {
+            letter = "F";
+        }
+
+        // Determine the + or - sign, unless it's an A or F
+        int lastDigit = grade % 10;
+
+        if (letter != "A" && letter != "F")
+        {
+            if (lastDigit >= 7)
+            {
+                sign = "+";
+            }
+            else if (lastDigit < 3)
+            {
+                sign = "-";
+            }
+        }
+        else if (letter == "A" && lastDigit < 3)
+        {
+            sign = "-"; // A- is valid
+        }
+        // F+ or F- not allowed, so no sign
+
+        // Display the final grade
+        Console.WriteLine($"Your grade is: {letter}{sign}");
+
+        // Check pass or fail
+        if (grade >= 70)
+        {
+            Console.WriteLine("Congratulations! You passed the class!");
+        }
+        else
+        {
+            Console.WriteLine("Don't worry, try again and you'll do better next time!");
+        }
     }
 }
--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -2,8 +2,49 @@ using System;
 
 class Program
 {
-    static void Main(string[] args)
+    static void Main()
     {
-        Console.WriteLine("Hello World! This is the Exercise3 Project.");
+        // Create a random number generator
+        Random randomGenerator = new Random();
+        string playAgain;
+
+        do
+        {
+            int magicNumber = randomGenerator.Next(1, 101); // Random number between 1 and 100
+            int guess = -1;
+            int guessCount = 0;
+
+            Console.WriteLine("I'm thinking of a number between 1 and 100...");
+
+            // Loop until the user guesses the correct number
+            while (guess != magicNumber)
+            {
+                Console.Write("What is your guess? ");
+                string guessInput = Console.ReadLine();
+                guess = int.Parse(guessInput);
+                guessCount++;
+
+                if (guess < magicNumber)
+                {
+                    Console.WriteLine("Higher");
+                }
+                else if (guess > magicNumber)
+                {
+                    Console.WriteLine("Lower");
+                }
+                else
+                {
+                    Console.WriteLine("You guessed it!");
+                    Console.WriteLine($"It took you {guessCount} guesses.");
+                }
+            }
+
+            // Ask if the user wants to play again
+            Console.Write("Do you want to play again? (yes/no): ");
+            playAgain = Console.ReadLine().ToLower();
+
+        } while (playAgain == "yes");
+
+        Console.WriteLine("Thanks for playing! Goodbye.");
     }
 }
